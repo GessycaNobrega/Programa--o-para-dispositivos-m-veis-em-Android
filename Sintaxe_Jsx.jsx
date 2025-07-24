12780d6c76206a27cc100777bf7de25661c5036c
GessycaNobrega <145810565+GessycaNobrega@users.noreply.github.com>
2025-07-24 20:22:10 -0300

    update

---
+++ Sintaxe_Jsx.jsx
@@ -0,0 +1,10 @@
+//Este fragmento de código mostra a forma de um elemento JSX:
+
+var element = (
+ <JSXElement>
+	 <SubJSXElement />
+	 <SubJSXElement />
+	 <SubJSXElement />
+ <JSXElement />
+);
+
