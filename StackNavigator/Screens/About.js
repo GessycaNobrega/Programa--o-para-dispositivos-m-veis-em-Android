b93d9f0dc6a06cacfe8a22011a732ceb9f8685e8
GessycaNobrega <145810565+GessycaNobrega@users.noreply.github.com>
2025-07-29 17:27:23 -0300

    update

---
+++ StackNavigator/Screens/About.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+ import { View, Text, Button } from 'react-native';
+
+ function AboutScreen({navigation}) {
+     return (
+     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+         <Text>About Screen</Text>
+         <Button title='Ir para Home' onPress={() => navigation.navigate('Home')} />
+     </View>
+     );
+ }
